7ffd8b3a17625495d87973696e7720fabc8dff98
zhaifuyin_motie <zhaifuyin@motie.com>
2017-09-27 18:26:15 +0800

    微信站初稿

---
+++ src/main/java/com/zhai/weixin/servive/WeChatService.java
@@ -0,0 +1,7 @@
+package com.zhai.weixin.servive;
+
+/**
+ * Created by admin on 2017/9/27.
+ */
+public class WeChatService {
+}
---
+++ src/main/java/com/zhai/weixin/web/WeiXinUrlController.java
@@ -0,0 +1,7 @@
+package com.zhai.weixin.web;
+
+/**
+ * Created by admin on 2017/9/27.
+ */
+public class WeiXinUrlController {
+}
