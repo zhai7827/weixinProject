cdd3770ade310322373d78884143d45b727437da
zhaifuyin_motie <zhaifuyin@motie.com>
2017-11-02 18:29:12 +0800

    微信站交互

--- src/main/java/com/zhai/weixin/servive/WeChatService.java
+++
@@ -1,7 +0,0 @@
-package com.zhai.weixin.servive;
-
-/**
- * Created by admin on 2017/9/27.
- */
-public class WeChatService {
-}
--- src/main/java/com/zhai/weixin/web/WeiXinUrlController.java
+++ src/main/java/com/zhai/weixin/web/WeiXinUrlController.java
@@ -1,7 +1,24 @@
 package com.zhai.weixin.web;
 
+import com.zhai.weixin.utils.CoreServlet;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by admin on 2017/9/27.
  */
+@Controller
+@RequestMapping(value="/weixin_wechat")
 public class WeiXinUrlController {
+    @RequestMapping(value="/wys")
+    public  void weixinNotice(HttpServletRequest request, HttpServletResponse response){
+        try {
+            boolean b = CoreServlet.messageReceive(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
